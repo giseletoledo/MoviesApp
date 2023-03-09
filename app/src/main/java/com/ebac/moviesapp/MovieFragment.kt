8a67d19a5349c11dddb2b0b799a59decfbd35e7f
gisele <gisesonia@gmail.com>
2023-03-09 00:03:42 -0300

    Refatorando para deixar o when mais legivel

--- app/src/main/java/com/ebac/moviesapp/MovieFragment.kt
+++ app/src/main/java/com/ebac/moviesapp/MovieFragment.kt
@@ -3,7 +3,6 @@ package com.ebac.moviesapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +11,14 @@ import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.ebac.moviesapp.databinding.FragmentItemListBinding
 
 class MovieFragment : Fragment(), MovieListener {
 
+    private lateinit var progressBar: ProgressBar
+    private lateinit var movieList: TextView
+    private lateinit var errorTextView: TextView
     private lateinit var adapter: MyMovieRecyclerViewAdapter
     private val viewModel by navGraphViewModels<MovieViewModel>(R.id.movie_graph) { defaultViewModelProviderFactory }
 
@@ -28,55 +31,51 @@ class MovieFragment : Fragment(), MovieListener {
         )
         val view = binding.root
 
+        val progressBar = binding.progressBar
+        val movieList = binding.list
+        val errorTextView = binding.errorTextView
+
         val layoutManager = LinearLayoutManager(context)
         adapter = MyMovieRecyclerViewAdapter(this)
 
-        binding.list.apply {
+        movieList.apply {
             this.adapter = this@MovieFragment.adapter
             this.layoutManager = layoutManager
         }
 
-        initObservers()
+        initObservers(progressBar, movieList, errorTextView)
 
         return view
     }
 
-  private fun initObservers(){
-      val progressBar = view?.findViewById<ProgressBar>(R.id.progressBar)
-      val movieList = view?.findViewById<TextView>(R.id.list)
-      val errorTextView = view?.findViewById<TextView>(R.id.errorTextView)
+  private fun initObservers(progressBar: ProgressBar,
+                            movieList: RecyclerView,
+                            errorTextView: TextView){
 
       viewModel.movieListLiveData.observe(viewLifecycleOwner, Observer {
         adapter.updateData(it)
       })
 
       viewModel.dataStateLiveData.observe(viewLifecycleOwner, Observer { state ->
-          progressBar?.let { progressBar ->
-              movieList?.let { movieDetailsTextView ->
-                  errorTextView?.let { errorTextView ->
-                      when (state) {
-                          DataState.LOADING -> {
-                              progressBar.visibility = View.VISIBLE
-                              movieDetailsTextView.visibility = View.GONE
-                              errorTextView.visibility = View.GONE
-                          }
-                          DataState.ERROR -> {
-                              progressBar.visibility = View.GONE
-                              movieDetailsTextView.visibility = View.GONE
-                              errorTextView.visibility = View.VISIBLE
-                              errorTextView.text = "Falha ao carregar a lista"
-                          }
-                          DataState.SUCCESS -> {
-                              progressBar.visibility = View.GONE
-                              movieDetailsTextView.visibility = View.VISIBLE
-                              errorTextView.visibility = View.GONE
-                          }
-                      }
-                  }
+          when (state) {
+              DataState.LOADING -> {
+                  progressBar.visibility = View.VISIBLE
+                  movieList.visibility = View.GONE
+                  errorTextView.visibility = View.GONE
+              }
+              DataState.ERROR -> {
+                  progressBar.visibility = View.GONE
+                  movieList.visibility = View.GONE
+                  errorTextView.visibility = View.VISIBLE
+                  errorTextView.text = "Falha ao carregar a lista"
+              }
+              DataState.SUCCESS -> {
+                  progressBar.visibility = View.GONE
+                  movieList.visibility = View.VISIBLE
+                  errorTextView.visibility = View.GONE
               }
           }
       })
-
 
       viewModel.navigationToDetailLiveData.observe(viewLifecycleOwner, Observer {
           val action = MovieFragmentDirections.actionMovieFragmentToMovieDetailsFragment()
